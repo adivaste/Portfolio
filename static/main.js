2a85e9575e45f87ec7de21302210753ee2966a50
adivaste <adivaste@outlook.com>
2022-08-28 14:57:21 +0530

    Added profile pic, short bio and updated logo

--- static/main.js
+++ static/main.js
@@ -3,11 +3,30 @@ function makeUnderline(li_id) {
       var li_element = document.getElementById(li_id);
       var li_elements = document.getElementsByClassName(nav_li_class_name);
 
-      console.log(li_elements)
+      // console.log(li_elements)
 
       for (var i = 0; i < li_elements.length; i++) {
             li_elements[i].classList.remove("active");
       }
       li_element.classList.add("active");
+
+      var element = document.querySelector(".content")
+      console.log(element)
+
+      loadHTML(element)
       
 }
+
+function processAjaxData(response, urlPath){
+      document.getElementById("content").innerHTML = response.html;
+      document.title = response.pageTitle;
+      window.history.pushState({"html":response.html,"pageTitle":response.pageTitle},"", urlPath);
+  }
+
+  function loadHTML(element){
+      fetch('./project.html')
+      .then(response=> response.text())
+      .then(text=> element.innerHTML = text);
+
+      
+    }
